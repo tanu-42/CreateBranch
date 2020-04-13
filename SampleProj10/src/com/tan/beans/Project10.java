c7b60242583add329cbbf0f89ef23f2535f1a7c3
TL <TL@gmail.com>
2020-04-13 15:29:22 +0530

    hey added first proj

---
+++ SampleProj10/src/com/tan/beans/Project10.java
@@ -0,0 +1,12 @@
+package com.tan.beans;
+
+public class Project10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello this is the tanoj kumar");
+		System.out.println("hello this is the tanoj kumar meesala");
+	}
+
+}
